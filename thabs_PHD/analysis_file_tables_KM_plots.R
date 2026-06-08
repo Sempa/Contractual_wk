d98385e8188a0667f9b161e132b5936a1bf1761c
Joseph B. Sempa <jbsempa@yahoo.co.uk>
2026-06-08 15:48:18 +0200

    Changing the threshold to 400 as requested by reviewers.

--- thabs_PHD/analysis_file_tables_KM_plots.R
+++ thabs_PHD/analysis_file_tables_KM_plots.R
@@ -28,25 +28,25 @@ final_dataset <- read_excel("hypo_alldata_wide_labels.xls") %>%
   filter(!is.na(UIN...1)) %>%
   mutate(
     # --- Primary AI classifications ---
-    addisons_disease = if_else(as.numeric(`Random cortisol result`) < 500, 1, 0),
+    addisons_disease = if_else(as.numeric(`Random cortisol result`) < 240, 1, 0),
     
     PAI = case_when(
-      as.numeric(`Random cortisol result`) < 500 &
-        as.numeric(`Synacthen: 30 minute cortisol result`) < 500 &
+      as.numeric(`Random cortisol result`) < 240 &
+        as.numeric(`Synacthen: 30 minute cortisol result`) < 240 &
         as.numeric(`ACTH result`) >= 64.7 & !is.na(`ACTH result`) ~ 1,
       TRUE ~ 0
     ),
     
     SAI = case_when(
-      as.numeric(`Random cortisol result`) < 500 &
-        as.numeric(`Synacthen: 30 minute cortisol result`) < 500 &
+      as.numeric(`Random cortisol result`) < 240 &
+        as.numeric(`Synacthen: 30 minute cortisol result`) < 240 &
         as.numeric(`ACTH result`) < 64.7 ~ 1,
       TRUE ~ 0
     ),
     
     total_AI = case_when(
-      as.numeric(`Random cortisol result`) < 500 & (PAI == 1 | SAI == 1) ~ 1,
-      as.numeric(`Random cortisol result`) < 500 & (PAI != 1 & SAI != 1) ~ 0,
+      as.numeric(`Random cortisol result`) < 240 & (PAI == 1 | SAI == 1) ~ 1,
+      as.numeric(`Random cortisol result`) < 240 & (PAI != 1 & SAI != 1) ~ 0,
       TRUE ~ NA_real_
     ),
     
@@ -254,7 +254,7 @@ final_dataset <- final_dataset %>%
   ) # %>%
 # mutate(`CD4 count` = as.numeric(`CD4 count`))
 x=final_dataset %>% filter(is.na(addisons_disease)) %>% dplyr::select(UIN...1, `Random cortisol result`, `ACTH result`, `Synacthen: 0 minute cortisol result`, `Synacthen: 30 minute cortisol result`)
-x=final_dataset %>% filter(`Random cortisol result`<500 & (!is.na(`Synacthen: 0 minute cortisol result`) | !is.na(`Synacthen: 30 minute cortisol result`))) %>% dplyr::select(UIN...1, `Random cortisol result`, `ACTH result`, `Synacthen: 0 minute cortisol result`, `Synacthen: 30 minute cortisol result`)
+x=final_dataset %>% filter(`Random cortisol result`<240 & (!is.na(`Synacthen: 0 minute cortisol result`) | !is.na(`Synacthen: 30 minute cortisol result`))) %>% dplyr::select(UIN...1, `Random cortisol result`, `ACTH result`, `Synacthen: 0 minute cortisol result`, `Synacthen: 30 minute cortisol result`)
 ##############################################################################################
 table_mortality <- final_dataset %>%
   dplyr::select(`Hospital folder number`, `3 followup_patientstatus`, `6 followup_patientstatus`, `12 followup_patientstatus`) %>%
@@ -483,8 +483,8 @@ km_fit2 <- survfit(Surv(time = ttdeath, event = mortality) ~ strata, data = dt04
 dt04_adj <- dt04 %>%
   mutate(strata = ifelse(strata != "No AI", "AI", strata),
          strata = ifelse(record_id %in% read_csv("AI_patient_list.csv")[[1]], "No AI", strata),
-         AI_strata = ifelse(record_id %in% (readr::read_csv("only_AI_participants_500.csv"))[[2]], "Stimulated", "non-Stimulated"
-                            # ifelse(!(record_id %in% (readr::read_csv("only_AI_participants_500.csv"))[[2]]) & strata != "No AI", "non-Stimulated", "")
+         AI_strata = ifelse(record_id %in% (readr::read_csv("only_AI_participants_240.csv"))[[2]], "Stimulated", "non-Stimulated"
+                            # ifelse(!(record_id %in% (readr::read_csv("only_AI_participants_240.csv"))[[2]]) & strata != "No AI", "non-Stimulated", "")
                             )
   )
 
@@ -562,7 +562,7 @@ annot_df <- data.frame(
 km_plot_manual <- ggplot(plot_data, aes(x = time_jittered, y = surv, color = strata)) +
   geom_step(size = 1.2) +
   geom_point(size = 1.5) +
-  labs(x = "Time Since Enrolment (months)", y = "Survival Probability", color = "Strata", title = 'A') +
+  labs(x = "Time Since Enrolment (months)", y = "Survival Probability", color = "Strata", title = 'B') +
   scale_color_manual(values = c("red", "darkgreen")) +
   scale_y_continuous(limits = c(0, 1), breaks = seq(0, 1, by = 0.25)) +
   scale_x_continuous(limits = c(0, 13), breaks = 0:13) +
@@ -594,16 +594,16 @@ km_plot_manual <- ggplot(plot_data, aes(x = time_jittered, y = surv, color = str
     )
   )
 
-km_plot_manual_500 <- km_plot_manual
+km_plot_manual_240 <- km_plot_manual
 
 final_stacked_plot <- cowplot::plot_grid(
-  km_plot_manual_500,
+  # km_plot_manual_500,
   km_plot_manual_400,
   km_plot_manual_240,
   ncol = 1
 )
 
-ggsave("KM_stacked_3x1.png",
+ggsave("KM_stacked_2x1.png",
        final_stacked_plot,
        width = 8,
        height = 8,
